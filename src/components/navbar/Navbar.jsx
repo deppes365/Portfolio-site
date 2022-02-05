49a94b35fd3c9ae5fbb01b51714de5bd800d36da
Darren Eppes <deppes365@gmail.com>
2022-02-04 19:53:54 -0500

    commit

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import './navbar.scss';
 import { NavLink } from 'react-router-dom';
-import { FaInstagram, FaGithub, FaLinkedinIn} from'react-icons/fa';
+import { FaInstagram, FaGithub, FaLinkedinIn } from 'react-icons/fa';
 
 function Navbar() {
-
 	function openMenu() {
-		document.querySelector('.hamburger').classList.toggle('open')
-		document.querySelector('.nav-links').classList.toggle('open')
+		document.querySelector('.hamburger').classList.toggle('open');
+		document.querySelector('.nav-links').classList.toggle('open');
 	}
 	function closeMenu() {
-		document.querySelector('.hamburger').classList.remove('open')
-		document.querySelector('.nav-links').classList.remove('open')
+		document.querySelector('.hamburger').classList.remove('open');
+		document.querySelector('.nav-links').classList.remove('open');
 	}
 
 	return (
@@ -23,16 +22,33 @@ function Navbar() {
 					</h1>
 				</div>
 				<div className="nav-links" onClick={closeMenu}>
-                    <NavLink to='/' className='nav-link' >Home</NavLink>
-                    <NavLink to='/portfolio' className='nav-link' >Portfolio</NavLink>
-					<NavLink to='/about' className='nav-link' >About</NavLink>
-                    <NavLink to='/contact' className='nav-link' >Contact</NavLink>
+					<NavLink to="/" className="nav-link">
+						Home
+					</NavLink>
+					<NavLink to="/portfolio" className="nav-link">
+						Portfolio
+					</NavLink>
+					<NavLink to="/about" className="nav-link">
+						About
+					</NavLink>
+					<NavLink to="/contact" className="nav-link">
+						Contact
+					</NavLink>
 					<div className="social-links">
-						<a href='https://www.instagram.com/darren.codes/' target='_blank'><FaInstagram/></a>
-						<a href="https://www.linkedin.com/in/darren-eppes-b71629230/" target='_blank'><FaLinkedinIn/></a>
-						<a href="https://github.com/deppes365/" target='_blank'><FaGithub/></a>
+						<a href="https://www.instagram.com/darren.codes/" target="_blank">
+							<FaInstagram />
+						</a>
+						<a
+							href="https://www.linkedin.com/in/darren-eppes-b71629230/"
+							target="_blank"
+						>
+							<FaLinkedinIn />
+						</a>
+						<a href="https://github.com/deppes365/" target="_blank">
+							<FaGithub />
+						</a>
 					</div>
-                </div>
+				</div>
 				<div className="hamburger" onClick={openMenu}>
 					<div className="top-line"></div>
 					<div className="middle-line"></div>
